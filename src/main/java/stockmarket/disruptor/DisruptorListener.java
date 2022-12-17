d01cc9449cd520ebf4748a8dffc4e1fdf7a04178
alexanastasyev <alex.anastasyev@mail.ru>
2022-12-18 00:07:21 +0300

    Added listeners for each currency pair

--- src/main/java/stockmarket/disruptor/DisruptorListener.java
+++ src/main/java/stockmarket/disruptor/DisruptorListener.java
@@ -1,31 +1,129 @@
 package stockmarket.disruptor;
 
 import com.lmax.disruptor.EventHandler;
+import exception.UnsupportedOrderTypeException;
 import model.CurrencyPair;
 import model.Order;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("DuplicatedCode")
 public class DisruptorListener implements EventHandler<OrderEvent> {
 
     private final CurrencyPair currencyPair;
 
     private final List<Order> orders;
-    private final Consumer<Order> processor;
 
-    public DisruptorListener(CurrencyPair currencyPair, Consumer<Order> processor) {
+    public DisruptorListener(CurrencyPair currencyPair) {
         this.currencyPair = currencyPair;
-        this.processor = processor;
         this.orders = new ArrayList<>();
     }
 
     @Override
-    public void onEvent(OrderEvent event, long sequence, boolean endOfBatch) throws Exception {
+    public void onEvent(OrderEvent event, long sequence, boolean endOfBatch) {
         OrderDto orderDto = event.getOrder();
         Order order = orderDto.getOrder();
-        processor.accept(order);
+        if (!order.getCurrencyPair().equals(currencyPair)) {
+            return;
+        }
+        matchOrder(order);
         orderDto.getOperationCompletedLatch().countDown();
+    }
+
+    public List<Order> getAllOrders() {
+        return new ArrayList<>(orders);
+    }
+
+    public void revokeAllOrders() {
+        orders.forEach(Order::revoke);
+        orders.clear();
+    }
+
+    private void matchOrder(Order order) {
+        List<Order> orderCandidates = orders.stream()
+                .filter(orderCandidate -> filterByClient(order, orderCandidate))
+                .filter(orderCandidate -> filterByType(order, orderCandidate))
+                .filter(orderCandidate -> filterByPrice(order, orderCandidate))
+                .sorted((order1, order2) -> compareOrdersForSorting(order1, order2, order))
+                .collect(Collectors.toList());
+
+        orderCandidates.forEach(orderCandidate -> {
+            BigDecimal dealAmount = orderCandidate.getAmount().min(order.getAmount());
+            BigDecimal dealPrice = getDealPrice(order, orderCandidate);
+
+            if (reduceOrder(orderCandidate, dealAmount, dealPrice)) {
+                closeOrder(orderCandidate);
+            }
+            if (reduceOrder(order, dealAmount, dealPrice)) {
+                order.revoke();
+            }
+        });
+
+        if (order.getAmount().compareTo(BigDecimal.ZERO) > 0) {
+            orders.add(order);
+        }
+    }
+
+    private boolean filterByClient(Order orderTarget, Order orderCandidate) {
+        return orderCandidate.getClient().getId() != orderTarget.getClient().getId();
+    }
+
+    private boolean filterByType(Order orderTarget, Order orderCandidate) {
+        return !orderCandidate.getOrderType().equals(orderTarget.getOrderType());
+    }
+
+    private boolean filterByPrice(Order orderTarget, Order orderCandidate) {
+        switch (orderTarget.getOrderType()) {
+            case BUY: {
+                return orderTarget.getPrice().compareTo(orderCandidate.getPrice()) >= 0;
+            }
+            case SELL: {
+                return orderTarget.getPrice().compareTo(orderCandidate.getPrice()) <= 0;
+            }
+            default: {
+                throw new UnsupportedOrderTypeException();
+            }
+        }
+    }
+
+    private int compareOrdersForSorting(Order order1, Order order2, Order orderTarget) {
+        switch (orderTarget.getOrderType()) {
+            case BUY: {
+                return order1.getPrice().compareTo(order2.getPrice());
+            }
+            case SELL: {
+                return (-1) * order1.getPrice().compareTo(order2.getPrice());
+            }
+            default: {
+                throw new UnsupportedOrderTypeException();
+            }
+        }
+    }
+
+    private BigDecimal getDealPrice(Order orderTarget, Order orderCandidate) {
+        switch (orderTarget.getOrderType()) {
+            case BUY: {
+                return orderTarget.getPrice().min(orderCandidate.getPrice());
+            }
+            case SELL: {
+                return orderTarget.getPrice().max(orderCandidate.getPrice());
+            }
+            default: {
+                throw new UnsupportedOrderTypeException();
+            }
+        }
+    }
+
+    private boolean reduceOrder(Order order, BigDecimal amount, BigDecimal price) {
+        order.reduce(amount, price);
+        return order.getAmount().compareTo(BigDecimal.ZERO) == 0;
+    }
+
+    private void closeOrder(Order order) {
+        order.revoke();
+        orders.remove(order);
     }
 }
--- src/main/java/stockmarket/disruptor/StockMarketDisruptor.java
+++ src/main/java/stockmarket/disruptor/StockMarketDisruptor.java
@@ -2,32 +2,27 @@ package stockmarket.disruptor;
 
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.util.DaemonThreadFactory;
-import exception.UnsupportedOrderTypeException;
 import model.CurrencyPair;
 import model.Order;
 import stockmarket.StockMarket;
 
-import java.math.BigDecimal;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("DuplicatedCode")
 public class StockMarketDisruptor implements StockMarket {
 
     private static final int BUFFER_SIZE = 2048;
 
-    private final Map<CurrencyPair, List<Order>> orders;
-
-    public Disruptor<OrderEvent> disruptor = new Disruptor<>(OrderEvent::new, BUFFER_SIZE, DaemonThreadFactory.INSTANCE);
+    private final Disruptor<OrderEvent> disruptor;
+    private final Map<CurrencyPair, DisruptorListener> pairListeners = new HashMap<>();
 
     public StockMarketDisruptor() {
-        this.orders = new ConcurrentHashMap<>(CurrencyPair.values().length);
+        disruptor = new Disruptor<>(OrderEvent::new, BUFFER_SIZE, DaemonThreadFactory.INSTANCE);
         for (CurrencyPair currencyPair : CurrencyPair.values()) {
-            orders.put(currencyPair, new ArrayList<>());
+            DisruptorListener listener = new DisruptorListener(currencyPair);
+            pairListeners.put(currencyPair, listener);
+            disruptor.handleEventsWith(listener);
         }
-
-        disruptor.handleEventsWith((event, sequence, endOfBatch) -> matchOrder(event.getOrder()));
         disruptor.start();
     }
 
@@ -46,100 +41,12 @@ public class StockMarketDisruptor implements StockMarket {
     @Override
     public List<Order> getAllOrdersList() {
         Set<Order> allOrdersSet = new HashSet<>();
-        orders.values().forEach(allOrdersSet::addAll);
+        pairListeners.values().forEach(listener -> allOrdersSet.addAll(listener.getAllOrders()));
         return new ArrayList<>(allOrdersSet);
     }
 
     @Override
     public void revokeAllOrders() {
-        getAllOrdersList().forEach(Order::revoke);
-        orders.values().forEach(List::clear);
-    }
-
-    private void matchOrder(OrderDto orderDto) {
-        List<Order> orderCandidates = orders.get(orderDto.getOrder().getCurrencyPair()).stream()
-                .filter(orderCandidate -> filterByClient(orderDto.getOrder(), orderCandidate))
-                .filter(orderCandidate -> filterByType(orderDto.getOrder(), orderCandidate))
-                .filter(orderCandidate -> filterByPrice(orderDto.getOrder(), orderCandidate))
-                .sorted((order1, order2) -> compareOrdersForSorting(order1, order2, orderDto.getOrder()))
-                .collect(Collectors.toList());
-
-        orderCandidates.forEach(orderCandidate -> {
-            BigDecimal dealAmount = orderCandidate.getAmount().min(orderDto.getOrder().getAmount());
-            BigDecimal dealPrice = getDealPrice(orderDto.getOrder(), orderCandidate);
-
-            if (reduceOrder(orderCandidate, dealAmount, dealPrice)) {
-                closeOrder(orderCandidate);
-            }
-            if (reduceOrder(orderDto.getOrder(), dealAmount, dealPrice)) {
-                orderDto.getOrder().revoke();
-            }
-        });
-
-        if (orderDto.getOrder().getAmount().compareTo(BigDecimal.ZERO) > 0) {
-            orders.get(orderDto.getOrder().getCurrencyPair()).add(orderDto.getOrder());
-        }
-
-        orderDto.getOperationCompletedLatch().countDown();
-    }
-
-    private boolean filterByClient(Order orderTarget, Order orderCandidate) {
-        return orderCandidate.getClient().getId() != orderTarget.getClient().getId();
-    }
-
-    private boolean filterByType(Order orderTarget, Order orderCandidate) {
-        return !orderCandidate.getOrderType().equals(orderTarget.getOrderType());
-    }
-
-    private boolean filterByPrice(Order orderTarget, Order orderCandidate) {
-        switch (orderTarget.getOrderType()) {
-            case BUY: {
-                return orderTarget.getPrice().compareTo(orderCandidate.getPrice()) >= 0;
-            }
-            case SELL: {
-                return orderTarget.getPrice().compareTo(orderCandidate.getPrice()) <= 0;
-            }
-            default: {
-                throw new UnsupportedOrderTypeException();
-            }
-        }
-    }
-
-    private int compareOrdersForSorting(Order order1, Order order2, Order orderTarget) {
-        switch (orderTarget.getOrderType()) {
-            case BUY: {
-                return order1.getPrice().compareTo(order2.getPrice());
-            }
-            case SELL: {
-                return (-1) * order1.getPrice().compareTo(order2.getPrice());
-            }
-            default: {
-                throw new UnsupportedOrderTypeException();
-            }
-        }
-    }
-
-    private BigDecimal getDealPrice(Order orderTarget, Order orderCandidate) {
-        switch (orderTarget.getOrderType()) {
-            case BUY: {
-                return orderTarget.getPrice().min(orderCandidate.getPrice());
-            }
-            case SELL: {
-                return orderTarget.getPrice().max(orderCandidate.getPrice());
-            }
-            default: {
-                throw new UnsupportedOrderTypeException();
-            }
-        }
-    }
-
-    private boolean reduceOrder(Order order, BigDecimal amount, BigDecimal price) {
-        order.reduce(amount, price);
-        return order.getAmount().compareTo(BigDecimal.ZERO) == 0;
-    }
-
-    private void closeOrder(Order order) {
-        order.revoke();
-        orders.get(order.getCurrencyPair()).remove(order);
+        pairListeners.values().forEach(DisruptorListener::revokeAllOrders);
     }
 }
